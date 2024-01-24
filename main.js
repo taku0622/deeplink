7d78b04f35d1cd63ee6fd0a4bc8d918ff9b77f73
taku <57785966+taku0622@users.noreply.github.com>
2024-01-24 17:19:22 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
-const alertHelloWorld = () => {
-  alert("HelloWorld");
-}
+const handleIOSDeepLink = () => {
+  if (window.opener) {
+    // Check if the page was opened by another app (e.g. Gmail, Teams)
+    window.close();
+    return;
+  }
+  const openedApp = window.open('https://apps.apple.com/jp/app/secom-sights/id6463053242', '_blank');
+  const checkAppInForeground = () => {
+    if (document.hidden) {
+      // Show App Store by URL
+      location.href = 'https://apps.apple.com/jp/app/secom-sights/id6463053242';
+    } else if (openedApp && !openedApp.closed) {
+      // If the application is available, open it and close tab
+      openedApp.close();
+    }
+  };
+  // Check if the app is in the foreground every 100 milliseconds
+  const checkInterval = setInterval(checkAppInForeground, 100);
+  // Clear the interval after a certain duration
+  const timeoutDuration = 5000; // Adjust the duration as needed
+  setTimeout(() => {
+    clearInterval(checkInterval);
+  }, timeoutDuration);
+};
