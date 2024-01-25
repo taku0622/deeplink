397ded08b86196c98d9057bd2dd29abcf7f4b6b2
taku0622 <hayatsutkm@gmail.com>
2024-01-25 18:23:07 +0900

    done

--- main.js
+++ main.js
@@ -11,9 +11,7 @@ const handleIOSDeepLink = () => {
   if (browser.name === 'Mobile Safari') {
     // リンクをクリック
     const openedApp = window.open('https://view.secomsights.com/login', '_blank');
-    if (Window.closed) {
-      window.alert('hey');
-    }
+    console.log(window);
     // var url = "https://deeplink-ashy.vercel.app/";
     // if (!window.open("https://apps.apple.com/jp/app/secom-sights/id6463053242")) {
     //   location.href = "https://apps.apple.com/jp/app/secom-sights/id6463053242";
