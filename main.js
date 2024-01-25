046d88b8619bedcdfa89318d73bdf810139a151d
taku0622 <hayatsutkm@gmail.com>
2024-01-25 17:10:42 +0900

    done

--- main.js
+++ main.js
@@ -7,27 +7,6 @@ const browser = ua.getBrowser();
 console.log(browser.name);
 
 const handleIOSDeepLink = () => {
-  // 
-  if (browser.name === 'Mobile Safari') {
-    // リンクをクリック
-    // window.open('https://deeplink-ashy.vercel.app/', "_blank");
-    var url = "https://deeplink-ashy.vercel.app/";
-    if (!window.open("https://apps.apple.com/jp/app/secom-sights/id6463053242")) {
-      location.href = "https://apps.apple.com/jp/app/secom-sights/id6463053242";
-      if (location.href === "https://apps.apple.com/jp/app/secom-sights/id6463053242") {
-        alert('nono')
-      }
-    }
-    // // local.hrefを確認
-    // setInterval(() => {
-    //   console.log(hello);
-    //   if (location.href === 'https://deeplink-ashy.vercel.app/') {
-    //     window.close();
-    //   }
-    // }, 1000);
-    return
-  }
-  // 
   if (window.opener) {
     // Check if the page was opened by another app (e.g. Gmail, Teams)
     window.close();
