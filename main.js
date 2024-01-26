450309a80da56abae18e33ea5740daaeddacca4c
taku0622 <hayatsutkm@gmail.com>
2024-01-26 15:53:04 +0900

    done

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 //宣言
 const ua = new UAParser();
 
-//ブラウザ情報取得
-const browser = ua.getBrowser();
-
-console.log(browser.name);
-
 const handleIOSDeepLink = () => {
-  const openedApp = window.open('https://view.secomsights.com/login', '_blank');
+  //ブラウザ情報取得
+  const browser = ua.getBrowser();
+
+  // ユーザアプリを開く(アプリ or 新規ウィンドウ)
+  const openedWindow = window.open('https://view.secomsights.com/login', '_blank');
+
   // safariのみ対応が異なるので分岐
   if (browser.name === 'Mobile Safari') {
-    if (openedApp) {
-      // ブラウザで遷移してしまう場合(≒アプリなし)、ストアへ遷移
-      openedApp.close();
+    if (openedWindow) {
+      // 新規ウィンドウに遷移してしまう場合(≒アプリなし)、アプリストアへ遷移
+      openedWindow.close();
       location.href = 'https://apps.apple.com/jp/app/secom-sights/id6463053242';
     }
     return;
@@ -20,11 +20,11 @@ const handleIOSDeepLink = () => {
   // safari以外
   const checkAppInForeground = () => {
     if (document.hidden) {
-      // アプリなしの場合、ページストアへ遷移
+      // アプリなしの場合、アプリストアへ遷移
       location.href = 'https://apps.apple.com/jp/app/secom-sights/id6463053242';
-    } else if (openedApp && !openedApp.closed) {
+    } else if (openedWindow && !openedWindow.closed) {
       // アプリありの場合、アプリを開いてタブを閉じます
-      openedApp.close();
+      openedWindow.close();
     }
   };
   // アプリがフォアグラウンドにあるか100ミリ秒ごとに確認
